fa4bad8e8d32f50837c6108de7107fe99e9015f9
LimTaeHun <lsw463@naver.com>
2023-06-12 19:04:14 +0900

    Feat: PlanetPool 클래스 화면 밖 생성 가능 영역 계산 구현

--- Assets/01.Scripts/Pool/PlanetPool.cs
+++ Assets/01.Scripts/Pool/PlanetPool.cs
@@ -6,6 +6,7 @@ public class PlanetPool : MonoBehaviour
     public Planet planetPrefab;
     public Transform parent;
 
+    public Vector2[,] spawnAbleAreas = new Vector2[4,2];
     public float minMass;
     public float maxMass;
     public float minSpeed;
@@ -14,8 +15,30 @@ public class PlanetPool : MonoBehaviour
     public List<Planet> PlanetPools { get; private set; }
     private const int PoolSize = 50;
 
+    private void SetSpawnAbleAreas()
+    {
+        Vector2 screenLeftBottom = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane));
+        
+        // 왼쪽 영역 왼쪽 아래, 오른쪽 위
+        spawnAbleAreas[0, 0] = new Vector2(screenLeftBottom.x - 1, screenLeftBottom.y - 1);
+        spawnAbleAreas[0, 1] = new Vector2(screenLeftBottom.x, (screenLeftBottom.y * -1) + 1);
+        
+        // 위쪽 영역 왼쪽 아래, 오른쪽 위
+        spawnAbleAreas[1, 0] = new Vector2(screenLeftBottom.x - 1, (screenLeftBottom.y * -1));
+        spawnAbleAreas[1, 1] = new Vector2((screenLeftBottom.x * -1) + 1, (screenLeftBottom.y * -1) + 1);
+        
+        // 오른쪽 영역 왼쪽 아래, 오른쪽 위
+        spawnAbleAreas[2, 0] = new Vector2((screenLeftBottom.x*-1), screenLeftBottom.y  - 1);
+        spawnAbleAreas[2, 1] = new Vector2((screenLeftBottom.x*-1)+1, (screenLeftBottom.y*-1) +1);
+        
+        // 아래 영역 왼쪽 아래, 오른쪽 위
+        spawnAbleAreas[3, 0] = new Vector2(screenLeftBottom.x - 1, screenLeftBottom.y - 1);
+        spawnAbleAreas[3, 1] = new Vector2((screenLeftBottom.x * -1) + 1, screenLeftBottom.y);
+    }
     public void SetPool()
     {
+        SetSpawnAbleAreas();
+        
         PlanetPools = new List<Planet>();
         for (int i = 0; i < PoolSize; i++)
         {
@@ -23,7 +46,6 @@ public class PlanetPool : MonoBehaviour
             PlanetPools.Add(planet);
         }
     }
-
     public void AdjustPlanetProperties(int wave)
     {
         minMass += wave * 1f;
@@ -31,14 +53,25 @@ public class PlanetPool : MonoBehaviour
         minSpeed += wave * 0.5f;
         maxSpeed += wave * 1f;
     }
-
     public void Spawn()
     {
         foreach (var planet in PlanetPools)
         {
             if (!planet.gameObject.activeSelf)
             {
-                planet.SetPlanet(Random.Range(minMass,maxMass),Random.Range(minSpeed,maxSpeed));
+                int spawnAreaIdx = Random.Range(0, 4);
+                
+                float spawnAreaMinX = spawnAbleAreas[spawnAreaIdx, 0].x;
+                float spawnAreaMaxX = spawnAbleAreas[spawnAreaIdx, 1].x;
+                float spawnAreaMinY = spawnAbleAreas[spawnAreaIdx, 0].y;
+                float spawnAreaMaxY = spawnAbleAreas[spawnAreaIdx, 1].y;
+
+                float randX = Random.Range(spawnAreaMinX, spawnAreaMaxX);
+                float randY = Random.Range(spawnAreaMinY, spawnAreaMaxY);
+                float mass = Random.Range(minMass, maxMass);
+                float speed = Random.Range(minSpeed, maxSpeed);
+
+                planet.SetPlanet(randX, randY, mass, speed);
                 planet.gameObject.SetActive(true);
                 break;
             }
