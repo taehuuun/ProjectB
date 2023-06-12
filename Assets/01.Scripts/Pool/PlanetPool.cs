c3acc100cc845c4bf4b8ae97269bfa2b824f2d3a
LimTaeHun <lsw463@naver.com>
2023-06-12 16:46:01 +0900

    Feat: PlanetPool 기능 구현

--- Assets/01.Scripts/Pool/PlanetPool.cs
+++ Assets/01.Scripts/Pool/PlanetPool.cs
@@ -1,5 +1,39 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlanetPool : MonoBehaviour
 {
+    public Planet planetPrefab;
+    public Transform parent;
+
+    public float minMass;
+    public float maxMass;
+    public float minSpeed;
+    public float maxSpeed;
+    
+    public List<Planet> PlanetPools { get; private set; }
+    private const int PoolSize = 50;
+
+    public void SetPool()
+    {
+        PlanetPools = new List<Planet>();
+        for (int i = 0; i < PoolSize; i++)
+        {
+            Planet planet = Instantiate(planetPrefab, parent, true);
+            planet.SetPlanet(Random.Range(minMass,maxMass),Random.Range(minSpeed,maxSpeed));
+            PlanetPools.Add(planet);
+        }
+    }
+
+    public void Spawn()
+    {
+        foreach (var planet in PlanetPools)
+        {
+            if (!planet.gameObject.activeSelf)
+            {
+                planet.gameObject.SetActive(true);
+                break;
+            }
+        }
+    }
 }
