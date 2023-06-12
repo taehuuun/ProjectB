e045a654a5b39b29cee9345102c56ffb8ff4b251
LimTaeHun <lsw463@naver.com>
2023-06-12 11:25:59 +0900

    Feat: BlackHole과 Planet의 기능 구현 및 GameManager의 기능 구현

--- Assets/01.Scripts/Managers/GameManager.cs
+++ Assets/01.Scripts/Managers/GameManager.cs
@@ -1,6 +1,25 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public BlackHole blackHole;
+
+    public List<Planet> planets;
+
+    private void Start()
+    {
+        foreach (var planet in planets)
+        {
+            planet.SetBlackHole(blackHole);
+        }
+    }
+    
+    private void Update()
+    {
+        foreach (var planet in planets)
+        {
+            blackHole.InteractPlanet(planet);
+        }
+    }
 }
--- Assets/01.Scripts/Planet/Planet.cs
+++ Assets/01.Scripts/Planet/Planet.cs
@@ -2,6 +2,22 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
+    public float Speed { get; private set; }
     public float Mass { get; private set; }
     public Vector2 Velocity { get;  set; }
+
+    private BlackHole _blackHole;
+
+    public void SetBlackHole(BlackHole blackHole)
+    {
+        _blackHole = blackHole;
+
+        Vector2 direction = (blackHole.transform.position - transform.position).normalized;
+        Velocity = direction * Speed;
+    }
+
+    private void Update()
+    {
+        transform.position += (Vector3)Velocity * Time.deltaTime;
+    }
 }
