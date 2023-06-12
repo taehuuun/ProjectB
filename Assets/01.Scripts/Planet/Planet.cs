e97d517a7891b1da7528761f80153469d9f2f022
LimTaeHun <lsw463@naver.com>
2023-06-12 09:34:23 +0900

    Refactor: Planet 클래스의 Velocity 프로퍼티 수정

--- Assets/01.Scripts/Planet/Planet.cs
+++ Assets/01.Scripts/Planet/Planet.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 public class Planet : MonoBehaviour
 {
     public float Mass { get; private set; }
-    public Vector2 Velocity { get; private set; }
+    public Vector2 Velocity { get;  set; }
 }
