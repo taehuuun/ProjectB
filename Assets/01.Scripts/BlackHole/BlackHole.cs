8f44acfe4987cc16f3abe7709d3e4669876a2bfe
LimTaeHun <lsw463@naver.com>
2023-06-12 09:42:02 +0900

    Feat: BlackHole의 InteractPlanet 메서드 구현

--- Assets/01.Scripts/BlackHole/BlackHole.cs
+++ Assets/01.Scripts/BlackHole/BlackHole.cs
@@ -3,4 +3,16 @@ using UnityEngine;
 public class BlackHole : MonoBehaviour
 {
     public float Mass { get; private set; }
+    public float EventHorizonRadius { get; private set; }
+
+    public void InteractPlanet(Planet planet)
+    {
+        float distance = Vector2.Distance(this.transform.position, planet.transform.position);
+
+        if (distance < EventHorizonRadius)
+        {
+            Vector2 direction = (this.transform.position - planet.transform.position).normalized;
+            planet.Velocity += direction * (Mass / (distance * distance));
+        }
+    }
 }
