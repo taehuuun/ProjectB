c65b8bcefbbfdd35ac9de032585252f01ebc5c2a
LimTaeHun <lsw463@naver.com>
2023-06-12 12:04:36 +0900

    Refactor: 값 테스트를 위한 코드 수정

--- Assets/01.Scripts/BlackHole/BlackHole.cs
+++ Assets/01.Scripts/BlackHole/BlackHole.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class BlackHole : MonoBehaviour
 {
-    public float Mass { get; private set; }
-    public float EventHorizonRadius { get; private set; }
+    [SerializeField] private float mass;
+
+    private const float GravityConst = 6.67430e-11f;
+    private const float SppedOfLight = 299792458f;
+
+    private float EventHorizonRadius => (2 * GravityConst * mass / (SppedOfLight * SppedOfLight));
 
     public void InteractPlanet(Planet planet)
     {
         float distance = Vector2.Distance(this.transform.position, planet.transform.position);
 
         Vector2 direction = (this.transform.position - planet.transform.position).normalized;
-        planet.Velocity += direction * (Mass / (distance * distance));
+        planet.Velocity += direction * (mass / (distance * distance));
         
         if (distance < EventHorizonRadius)
         {
