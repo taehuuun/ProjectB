b7731755f290e774efdb04969e06b9a893de815b
LimTaeHun <lsw463@naver.com>
2023-06-14 16:21:23 +0900

    Fix: Influence Radius 와 LineRenderer의 범위가 일치 하지 않는 문제 수정

--- Assets/01.Scripts/BlackHole/BlackHole.cs
+++ Assets/01.Scripts/BlackHole/BlackHole.cs
@@ -2,43 +2,62 @@ using UnityEngine;
 
 public class BlackHole : MonoBehaviour
 {
-    [SerializeField] private Transform influenceRadius;
+    [SerializeField] private Transform blackHoleSprite;
+    [SerializeField] private LineRenderer lineRenderer;
+    
     [SerializeField] private float mass;
     [SerializeField] private float influenceRadiusValue;
     
-    private const float GravityConst = 1f;
-    private const float SpeedOfLight = 3f;
+    private const float GravityConst = 5f;
+    private const float SpeedOfLight = 30f;
 
-    private float EventHorizonRadius => (2 * GravityConst * mass / (SpeedOfLight * SpeedOfLight));
-
-    private void Start()
-    {
-        Debug.Log(EventHorizonRadius);
-    }
+    private float EventHorizonRadius => (2 * GravityConst * mass / SpeedOfLight);
 
     private void Update()
     {
-        float scaledRadius = EventHorizonRadius;
-        transform.localScale = new Vector3(scaledRadius, scaledRadius, scaledRadius);
-        
-        influenceRadius.localScale = new Vector3(influenceRadiusValue, influenceRadiusValue, 1);
+        DrawInfluenceRadius(influenceRadiusValue);
+        blackHoleSprite.localScale = new Vector3(EventHorizonRadius, EventHorizonRadius, 1);
     }
     
     public void InteractPlanet(Planet planet)
     {
         float distance = Vector2.Distance(this.transform.position, planet.transform.position);
 
-        if (distance < influenceRadiusValue * 0.7f)
+        if (distance > influenceRadiusValue)
         {
-            Vector2 direction = (this.transform.position - planet.transform.position).normalized;
-            float force = (GravityConst * mass * planet.Mass) /  (distance * distance);
-            planet.Velocity += direction * (force * 0.5f);
+            return;
+        }
+        
+        Vector2 direction = (this.transform.position - planet.transform.position).normalized;
+        
+        float force = (GravityConst * mass * planet.Mass) /  (distance * distance);
+        planet.Velocity += direction * (force * 0.5f);
 
-            if (distance < EventHorizonRadius)
-            {
-                planet.gameObject.SetActive(false);
-                Debug.Log("흡수 됨");
-            }
+        if (distance < EventHorizonRadius)
+        {
+            planet.gameObject.SetActive(false);
+            Debug.Log("흡수 됨");
+        }
+    }
+
+    private void DrawInfluenceRadius(float radius)
+    {
+        int segments = 50;
+        lineRenderer.positionCount = segments + 1;
+        lineRenderer.useWorldSpace = true;
+        lineRenderer.startWidth = 0.01f;
+        lineRenderer.endWidth = 0.01f;
+
+        float angle = 0f;
+        
+        for (int i = 0; i <= segments; i++)
+        {
+            float x = Mathf.Sin(Mathf.Deg2Rad * angle) * radius; 
+            float y = Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
+
+            lineRenderer.SetPosition(i,  transform.position + new Vector3(x, y, 0));
+        
+            angle += 360f / segments;
         }
     }
 }
