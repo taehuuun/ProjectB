f971a8d594a3667496e0bcee334798fac9f710a3
LimTaeHun <lsw463@naver.com>
2023-06-18 00:01:28 +0900

    Add: GlobalSettings 정적 클래스에 프로젝트 전반에 사용할 정적 상수 값들 추가

--- Assets/01.Scripts/Config/GlobalSettings.cs
+++ Assets/01.Scripts/Config/GlobalSettings.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GlobalSettings : MonoBehaviour
+public static class GlobalSettings
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Initial costs
+    public static float InitialBlackHoleMassDecreaseCost = 100.0f;
+    public static float InitialBlackHoleRadiusIncreaseCost = 100.0f;
+    public static float InitialPlanetMassAbsorptionIncreaseCost = 100.0f;
+    public static float InitialWaveDurationDecreaseCost = 100.0f;
+    public static float InitialWaveWaitTimeIncreaseCost = 100.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Cost multipliers
+    public static float BlackHoleMassDecreaseCostMultiplier = 1.5f;
+    public static float BlackHoleRadiusIncreaseCostMultiplier = 1.5f;
+    public static float PlanetMassAbsorptionIncreaseCostMultiplier = 1.5f;
+    public static float WaveDurationDecreaseCostMultiplier = 1.5f;
+    public static float WaveWaitTimeIncreaseCostMultiplier = 1.5f;
+
+    // Upgrade amounts
+    public static float BlackHoleMassDecreaseAmount = 0.01f;
+    public static float BlackHoleRadiusIncreaseAmount = 0.1f;
+    public static float PlanetMassAbsorptionIncreaseAmount = 0.1f;
+    public static float WaveDurationDecreaseAmount = 1.0f;
+    public static float WaveWaitTimeIncreaseAmount = 1.0f;
 }
