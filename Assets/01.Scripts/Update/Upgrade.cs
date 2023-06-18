f7dc777007eaa342230ceccfaa53a4f8c1a94f44
LimTaeHun <lsw463@naver.com>
2023-06-18 10:55:04 +0900

    Feat: Upgrade 필드 및 생성자, Apply 메서드 구현

--- Assets/01.Scripts/Update/Upgrade.cs
+++ Assets/01.Scripts/Update/Upgrade.cs
@@ -2,4 +2,24 @@ using UnityEngine;
 
 public class Upgrade : MonoBehaviour
 {
+    public string name;
+    public float cost;
+    public float costMultiplier;
+    public float amount;
+    public int level;
+
+    public Upgrade(string name, float cost, float costMultiplier, float amount, int level)
+    {
+        this.name = name;
+        this.cost = cost;
+        this.costMultiplier = costMultiplier;
+        this.amount = amount;
+        this.level = level;
+    }
+
+    public void Apply()
+    {
+        cost *= costMultiplier;
+        level++;
+    }
 }
