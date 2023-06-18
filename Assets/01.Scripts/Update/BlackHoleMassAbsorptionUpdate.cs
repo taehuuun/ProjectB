032d9ec60fce5c72ce58124a9a7b9af05d7c8a8e
LimTaeHun <lsw463@naver.com>
2023-06-18 19:34:43 +0900

    Feat: BlackHoleMassAbsorptionUpdate클래스 Update 기능 구현

--- Assets/01.Scripts/Update/BlackHoleMassAbsorptionUpdate.cs
+++ Assets/01.Scripts/Update/BlackHoleMassAbsorptionUpdate.cs
@@ -1,5 +1,17 @@
-using UnityEngine;
-
-public class BlackHoleMassAbsorptionUpdate : MonoBehaviour
+public class BlackHoleMassAbsorptionUpdate : Upgrade
 {
+    private BlackHole _blackHole;
+    public BlackHoleMassAbsorptionUpdate(BlackHole blackHole) : 
+        base("블랙홀 질량 흡수량",
+            GlobalSettings.InitialPlanetMassAbsorptionIncreaseCost,
+            GlobalSettings.BlackHoleMassAbsorptionIncreaseCostMultiplier,
+            GlobalSettings.BlackHoleMassAbsorptionIncreaseCostMultiplier)
+    {
+        _blackHole = blackHole;
+    }
+
+    public override void ApplyUpgrade()
+    {
+        _blackHole.MassAbsorption += amount;
+    }
 }
