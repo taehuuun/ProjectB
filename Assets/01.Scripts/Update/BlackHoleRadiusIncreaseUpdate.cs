ec3ec7988d9162dcc9b6bcb7275118d62393fd71
LimTaeHun <lsw463@naver.com>
2023-06-18 19:03:35 +0900

    Feat: BlackHoleRadiusIncreaseUpdate 클래스 Update 기능 구현

--- Assets/01.Scripts/Update/BlackHoleRadiusIncreaseUpdate.cs
+++ Assets/01.Scripts/Update/BlackHoleRadiusIncreaseUpdate.cs
@@ -1,5 +1,17 @@
-using UnityEngine;
-
-public class BlackHoleRadiusIncreaseUpdate : MonoBehaviour
+public class BlackHoleRadiusIncreaseUpdate : Upgrade
 {
+    private BlackHole _blackHole;
+    public BlackHoleRadiusIncreaseUpdate(BlackHole blackHole) : 
+        base("블랙홀 중력 영향 범위 증가",
+            GlobalSettings.InitialBlackHoleRadiusIncreaseCost,
+            GlobalSettings.BlackHoleRadiusIncreaseCostMultiplier,
+            GlobalSettings.BlackHoleRadiusIncreaseAmount)
+    {
+        _blackHole = blackHole;
+    }
+
+    public override void ApplyUpgrade()
+    {
+        _blackHole.InfluenceRadiusValue += amount;
+    }
 }
