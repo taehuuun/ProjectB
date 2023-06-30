6b3d203a8858fdf2d37b288b4a14adf17eb83fcd
LimTaeHun <lsw463@naver.com>
2023-06-30 22:34:07 +0900

    Feat: UpgradeButton 필요 기능 구현

--- Assets/01.Scripts/UI/Buttons/UpgradeButton.cs
+++ Assets/01.Scripts/UI/Buttons/UpgradeButton.cs
@@ -1,6 +1,30 @@
+using TMPro;
 using UnityEngine;
 
 public class UpgradeButton : MonoBehaviour
 {
-   
+   [SerializeField] private TMP_Text upgradeNameText;
+   [SerializeField] private TMP_Text levelText;
+   [SerializeField] private TMP_Text costText;
+
+   private Upgrade _linkedUpgrade;
+
+   public void SetUpgrade(Upgrade upgrade)
+   {
+      _linkedUpgrade = upgrade;
+      UpdateUI();
+   }
+
+   public void OnButtonClick()
+   {
+      _linkedUpgrade.PurchaseUpdate();
+      UpdateUI();
+   }
+
+   private void UpdateUI()
+   {
+      upgradeNameText.text = levelText.name;
+      levelText.text = $"Lv.{_linkedUpgrade.level.ToString()}";
+      costText.text = $"$ {_linkedUpgrade.cost.ToString()}";
+   }
 }
